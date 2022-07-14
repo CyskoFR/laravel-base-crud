e2e78850134defcf0ea8ecabae2e814c107783d5
CyskoFR <franc.sko91@gmail.com>
2022-07-14 16:52:32 +0200

    add bootstrap + views

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -6,5 +6,31 @@
 
 @section('page-content')
     <h1>Comics list:</h1>
-    
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Description</th>
+                <th scope="col">Thumb</th>
+                <th scope="col">Type</th>
+                <th scope="col">Price$</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($comics as $comic)
+                <tr>
+                    <td>{{$comic->id}}</td>
+                    <td>{{$comic->title}}</td>
+                    <td>{{$comic->description}}</td>
+                    <td><img src="{{$comic->thumb}}" alt="{{$comic->title}}"></td>
+                    <td>{{$comic->type}}</td>
+                    <td>{{$comic->price}}</td>
+                    <td>
+                        <a class="btn btn-light" href="">More</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
